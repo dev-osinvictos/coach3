e400d0f02d40502b426332dda712a0e990a922a9
dev-osinvictos <dev@osinvictos.com.br>
2025-10-19 13:21:29 -0300

    migrando

--- server.js
+++ server.js
@@ -10,6 +10,23 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.static("public"));
 
+app.get("/config", (req, res) => {
+  res.json({
+    firebaseConfig: {
+      apiKey: process.env.FIREBASE_API_KEY,
+      authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+      projectId: process.env.FIREBASE_PROJECT_ID,
+      storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+      messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+      appId: process.env.FIREBASE_APP_ID,
+    },
+    supabaseConfig: {
+      url: process.env.SUPABASE_URL,
+      anonKey: process.env.SUPABASE_ANON_KEY,
+    },
+  });
+});
+
 app.get("/", (req, res) => {
   const htmlPath = path.join(process.cwd(), "index.html");
   let html = fs.readFileSync(htmlPath, "utf-8");
